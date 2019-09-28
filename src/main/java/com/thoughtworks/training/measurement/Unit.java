3b899ae3e1d23e9bf8117b554ae4989a686ab080
dipakkailaspatil <patil.dipak363@gmail.com>
2019-09-28 19:10:38 +0530

    [dipak | swapnil] Refactor. remove duplication by adding constructor to the Enum

--- src/main/java/com/thoughtworks/training/measurement/Unit.java
+++ src/main/java/com/thoughtworks/training/measurement/Unit.java
@@ -1,20 +1,15 @@
 package com.thoughtworks.training.measurement;
 
 public enum Unit {
-    //return value as it is
-    INCH {
-        @Override
-        double conversionToBase(double value) {
-            return value * 1;
-        }
-    },
-    // convert value to inch and return
-    FOOT {
-        @Override
-        double conversionToBase(double value) {
-            return value * 12;
-        }
-    };
+    INCH(1), FOOT(12);
 
-    abstract double conversionToBase(double value);
+    private double conversionFactor;
+
+    Unit(double conversionFactor) {
+        this.conversionFactor = conversionFactor;
+    }
+
+    double conversionToBase(double value) {
+        return value * conversionFactor;
+    }
 }
