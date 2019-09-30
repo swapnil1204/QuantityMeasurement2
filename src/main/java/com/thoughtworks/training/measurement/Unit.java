d89f38f823874668cacb5f38b17cb95ad2226a42
swapnilvijaykumarbamb <swapnilbamb1204@gmail.com>
2019-09-30 16:19:18 +0530

    [ swapnil ] Refactor. remove unnecessary field

--- src/main/java/com/thoughtworks/training/measurement/Unit.java
+++ src/main/java/com/thoughtworks/training/measurement/Unit.java
@@ -5,7 +5,6 @@ public enum Unit {
     INCH(1), FOOT(12), YARD(36), GALLON(3.78),
     LITER(1);
 
-    private String type;
     private double conversionFactor;
 
     Unit(double conversionFactor) {
